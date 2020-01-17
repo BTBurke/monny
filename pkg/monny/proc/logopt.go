360ae2568fe27c15aab0ca0c3f52b04b3964f342
Bryan Burke <bryan.t.burke@gmail.com>
2020-01-17 16:31:34 -0500

    adds additional log processor options to configure sources and sinks

--- pkg/monny/proc/logopt.go
+++ pkg/monny/proc/logopt.go
@@ -1,7 +1,9 @@
 package proc
 
 import (
+	"fmt"
 	"io"
+	"os"
 	"os/exec"
 )
 
@@ -13,12 +15,32 @@ type logProcOpt struct {
 
 type sourceOrSink int
 
+// all possible sources and sinks, pX means the wrapped or piped process channels
+// mX indicates the monny process's input/output.  Default would be to process logs from
+// pStdout->mStdout, pStderr->mStderr, but can be configured different ways depending on
+// how monny is run in relation to the monitored process
 const (
 	_ sourceOrSink = iota
-	stdout
-	stderr
-	stdin
+	pStdout
+	pStderr
+	pStdin
+	mStdout
+	mStderr
+	mStdin
 	logfile
+)
+
+// option priority matters for overriding default behavior
+type priority int
+
+const (
+	history priority = iota
+	command
+	noStdoutOut
+	noStderrOut
+	noOutput
+	noStdoutIn
+	noStderrIn
 )
 
 type source struct {
@@ -29,36 +51,158 @@ type source struct {
 
 type sink struct {
 	name sourceOrSink
-	out  []io.WriteCloser
+	out  io.WriteCloser
 }
 
+// LogProcessorOption overrides default behavior.  Options are applied in a defined order
+// no matter the order in which they are passed to the constructor so that principle of least
+// surprise applies.
 type LogProcessorOption interface {
-	apply(*logProcOpt)
-	priority() int
+	apply(*logProcOpt) error
+	priority() priority
 }
 
+// WithHistory controls how much of the most recent log source is retained and reported when
+// an alert is generated.  It applies by default to all configured log sources (e.g., to Stdout and
+// Stderr if both sources are configured)
 func WithHistory(hist int) LogProcessorOption {
 	return optF{
-		f:   func(l *logProcOpt) { l.hist = hist },
-		pri: 0,
+		f:   func(l *logProcOpt) error { l.hist = hist; return nil },
+		pri: history,
 	}
 }
 
+// options should return an optF struct to apply the option and declare its priority to the constructor
 type optF struct {
-	f   func(*logProcOpt)
-	pri int
+	f   func(*logProcOpt) error
+	pri priority
 }
 
-func (o optF) apply(l *logProcOpt) { o.f(l) }
-func (o optF) priority() int       { return o.pri }
+func (o optF) apply(l *logProcOpt) error { return o.f(l) }
+func (o optF) priority() priority        { return o.pri }
 
+// WithCommand will configure log sources and sinks based on the defined command.  This will determine which
+// log sources should be parsed depending on whether monny wraps a forked process or is run as a piped process
+// from an earlier command.  Other options may override default behavior.  Default behavior is to copy sources
+// directly to the same sink (Stdout->Stdout, Stderr->Stderr) when wrapping a forked process.  When run via a previous
+// piped command, monny will parse logs on Stdin and sink to Stdout.  Note that piped processes lose Stderr information.
+// Output sinks can be changed by other options, such as using rotated log files.
 func WithCommand(c *exec.Cmd) LogProcessorOption {
-	f := func(l *logProcOpt) {
-		// empty path indicates monny running as piped final process, stdin pipe should be treated as the source
+	f := func(l *logProcOpt) error {
+		switch {
+		// empty path indicates monny running as piped final process, stdin should be treated as the source
 		// and defaults to a stdout sink unless overridden subsequently by a sink-related option
+		case c.Path == "":
+			l.sources = append(l.sources, source{
+				name: mStdin,
+				q:    NewQueue(l.hist),
+				in:   os.Stdin,
+			})
+			l.sinks = append(l.sinks, sink{
+				name: mStdout,
+				out:  os.Stdout,
+			})
+		// TODO: must handle the piped process to pass previous command stdin to wrapped command
+		// in the command processor
+		default:
+			outPipe, err := c.StdoutPipe()
+			if err != nil {
+				return fmt.Errorf("unable to get process stdout: %v", err)
+			}
+			errPipe, err := c.StderrPipe()
+			if err != nil {
+				return fmt.Errorf("unable to get process stderr: %v", err)
+			}
+			l.sources = append(l.sources, source{
+				name: pStdout,
+				q:    NewQueue(l.hist),
+				in:   outPipe,
+			}, source{
+				name: pStderr,
+				q:    NewQueue(l.hist),
+				in:   errPipe,
+			})
+			l.sinks = append(l.sinks, sink{
+				name: mStdout,
+				out:  os.Stdout,
+			}, sink{
+				name: mStderr,
+				out:  os.Stderr,
+			})
+		}
+		return nil
 	}
 	return optF{
 		f:   f,
-		pri: 0,
+		pri: command,
 	}
+}
+
+// WithNoOutput prevents monny from echoing processed logs to either Stdout
+// or Stderr
+func WithNoOutput() LogProcessorOption {
+	return optF{
+		f:   func(l *logProcOpt) error { l.sinks = []sink{}; return nil },
+		pri: noOutput,
+	}
+}
+
+// WithNoStderrOutput prevents monny from echoing processed logs on Stderr
+func WithNoStderrOutput() LogProcessorOption {
+	return optF{
+		f:   func(l *logProcOpt) error { l.sinks = filterSink(l.sinks, mStderr); return nil },
+		pri: noStderrOut,
+	}
+}
+
+// WithNoStdoutOutput prevents monny from echoing processed logs on Stdout
+func WithNoStdoutOutput() LogProcessorOption {
+	return optF{
+		f:   func(l *logProcOpt) error { l.sinks = filterSink(l.sinks, mStdout); return nil },
+		pri: noStdoutOut,
+	}
+}
+
+// WithNoStdoutInput prevents monny from processing logs from your process's Stdout
+func WithNoStdoutInput() LogProcessorOption {
+	return optF{
+		f:   func(l *logProcOpt) error { l.sources = filterSource(l.sources, pStdout); return nil },
+		pri: noStdoutIn,
+	}
+}
+
+// WithNoStderrInput prevents monny from processing logs from your process's Stderr
+func WithNoStderrInput() LogProcessorOption {
+	return optF{
+		f:   func(l *logProcOpt) error { l.sources = filterSource(l.sources, pStderr); return nil },
+		pri: noStderrIn,
+	}
+}
+
+// filter one sink from the default configured sinks
+func filterSink(sinks []sink, target sourceOrSink) []sink {
+	fSinks := []sink{}
+	for _, s := range sinks {
+		switch s.name {
+		case target:
+			continue
+		default:
+			fSinks = append(fSinks, s)
+		}
+	}
+	return fSinks
+}
+
+// filter one source from the default configured sources
+func filterSource(sources []source, target sourceOrSink) []source {
+	fSources := []source{}
+	for _, s := range sources {
+		switch s.name {
+		case target:
+			continue
+		default:
+			fSources = append(fSources, s)
+		}
+	}
+	return fSources
 }
